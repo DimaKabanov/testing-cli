416fd283c1132781981fed42a29e6713d514c2d6
Dmitry Kabanov <dkabanov6@gmail.com>
2019-03-09 18:00:16 +0500

    update tests

--- __tests__/weather.test.js
+++ __tests__/weather.test.js
@@ -1,21 +1,21 @@
-import '@babel/polyfill';
 import { promises as fs } from 'fs';
+import assert from 'power-assert';
 import nock from 'nock';
-import weather from '../src';
+import getWeather from '../src';
 
 describe('Get weather data by city name', () => {
   test('Correct weather for London', async () => {
     const testWeatherDataPath = '__tests__/__fixtures__/weather.json';
     const weatherData = await fs.readFile(testWeatherDataPath, { encoding: 'utf8' });
-    const host = 'http://api.openweathermap.org/';
+    const host = 'https://api.openweathermap.org/';
     const url = 'data/2.5/weather?q=London,us&APPID=623ee6fdfa2383b014a27272039db110';
     nock(host)
       .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
       .get(`/${url}`)
       .reply(200, weatherData);
 
-    const actual = await weather('London');
-    const expected = 'Temperature in London 280.2 F';
-    expect(actual).toBe(expected);
+    const actual = await getWeather('London');
+    const expected = 280.2;
+    assert.strictEqual(actual, expected);
   });
 });
